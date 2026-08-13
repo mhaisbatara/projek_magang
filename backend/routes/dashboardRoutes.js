0f34a524143c53e15c997d35342ca9b904ce4905
M. Hais Batara <batarahais24@gmail.com>
2026-08-13 13:50:13 +0700

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDashboardSummary } from "../controllers/dashboardController.js";
-
-const router = express.Router();
-
-router.get("/summary", getDashboardSummary);
-
-export default router;
